c6cc9cbe5a810502f8f4b079d209829eecee6c83
DevendraPPatil <devendra.patil@tekditechnologies.com>
2024-04-26 17:15:12 +0530

    logger removed.

--- src/mongo_module/modules/virtual_id/virtual_id.router.ts
+++ src/mongo_module/modules/virtual_id/virtual_id.router.ts
@@ -1,6 +1,5 @@
 import { Router } from "express";
 import virtualIdController from "./virtual_id.controller";
-
 const virtualIRouter = Router();
 
 virtualIRouter.post("/generateVirtualID", virtualIdController.genarateVirtualId);
