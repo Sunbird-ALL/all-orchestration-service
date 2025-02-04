29b0926a07c69d8f2e9dddcf7b242238ef0fd348
SethuK-ND <sethu@navadhiti.com>
2025-02-04 15:08:09 +0530

    review token verification in all router

--- src/mongo_module/modules/lesson/lesson.router.ts
+++ src/mongo_module/modules/lesson/lesson.router.ts
@@ -1,11 +1,10 @@
 import { Router } from "express";
-import lessonController from "./lesson.controller";
 import verifyToken from "../../middlewares/verify.token";
-
+import lessonController from "./lesson.controller";
 
 const lessonRouter = Router();
 
-lessonRouter.post("/addLesson",verifyToken ,lessonController.addLesson);
+lessonRouter.post("/addLesson", verifyToken, lessonController.addLesson);
 
 lessonRouter.get("/getLessonProgressByUserId", verifyToken, lessonController.getLessonProgress);
 
