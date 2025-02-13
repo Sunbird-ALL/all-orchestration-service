1104100886f14d41c14a5bf0617e7f336e448187
SethuK-ND <sethu@navadhiti.com>
2025-02-13 10:14:07 +0530

    fix validation issues in user id field

--- src/mongo_module/validates/learner_progress.validate.ts
+++ src/mongo_module/validates/learner_progress.validate.ts
@@ -1,6 +1,6 @@
 import Joi from 'joi';
 
-const userId = Joi.string().trim().required();
+const userId = Joi.number().integer().required();
 const sessionId = Joi.string().trim().required().empty();
 const subSessionId = Joi.string().trim().required().empty();
 const milestoneLevel = Joi.string().trim().required().empty();
