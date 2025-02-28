713f85cbf120aa820f2541de7c2dfdeb1e9c48ff
DevendraPPatil <devendra.patil@tekditechnologies.com>
2025-02-28 14:45:23 +0530

    Fix: point and lesson validation added

--- src/mongo_module/validates/point.validate.ts
+++ src/mongo_module/validates/point.validate.ts
@@ -4,11 +4,11 @@ const userId = Joi.number().integer().required();
 const sessionId = Joi.string().trim().required().empty();
 const language = Joi.string()
     .trim()
-    .valid('en','kn','tn','hi','gu','te' , 'or')
+    .valid('en','kn','tn','hi','gu','te','or')
     .required();
 
 const milestone = Joi.string().trim().required().empty();
-const points = Joi.number().required();
+const points = Joi.number().min(0).max(100).required();
 
 const getPointsByUserIdValidationSchema = Joi.object({
     userId: userId,
