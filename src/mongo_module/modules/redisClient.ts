6e1eaacfbb79f75f4cc1cdc789bb1cc40253b358
DevendraPPatil <devendra.patil@tekditechnologies.com>
2025-06-12 12:04:06 +0530

    Feat: Password added for the redis

--- src/mongo_module/modules/redisClient.ts
+++ src/mongo_module/modules/redisClient.ts
@@ -5,6 +5,7 @@ const redisClient = createClient({
     host: process.env.REDIS_HOST || '127.0.0.1',
     port: parseInt(process.env.REDIS_PORT || '6379', 10)
   },
+  password: process.env.REDIS_PASSWORD,
 });
 
 redisClient.on('error', (err) => {
